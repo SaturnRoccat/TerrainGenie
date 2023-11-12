7b55e7e72cc77ac91c1607682bdffd452803fc7b
Duckos-Mods <businwes@hotmail.com>
2023-11-12 18:14:20 +0000

    added handling of the size param and added the beginings of the JS builder

--- main/main.go
+++ main/main.go
@@ -9,12 +9,24 @@ import (
 func main() {
 	fmt.Println("Welcome to Terrain Genie!")
 	var config TG_Config
+	var generateJS bool = false
 
 	flag.StringVar(&config.OutputPath, "output", "./levelData.bin", "Output path for generated terrain data")
 	flag.IntVar(&config.Seed, "seed", rand.Int(), "Seed for random number generator")
+	flag.IntVar(&config.XSize, "X", 8, "The size in CHUNKS of the X axis of the world")
+	flag.IntVar(&config.ZSize, "Z", 8, "The size in CHUNKS of the Z axis of the world")
+	flag.IntVar(&config.YSize, "Y", 300, "The size in BLOCKS of the Y axis of the world")
+
 	flag.Parse()
 
 	fmt.Println("Output path:", config.OutputPath)
 	fmt.Println("Seed:", config.Seed)
+
+	if generateJS {
+		fmt.Println("Generating JavaScript...")
+	} else {
+		fmt.Println("Generating binary of world data this will take some time...")
+		buildDataBuffer(config)
+	}
 
 }
