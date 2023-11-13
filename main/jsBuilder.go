b25863275945ab081c1dbb5076fc811dc03d4add
Duckos-Mods <businwes@hotmail.com>
2023-11-13 02:21:27 +0000

    rewrote the conversion to JS data. It has less of a chance to explode. Should be faster And only uses some unsafe pointers

--- main/jsBuilder.go
+++ main/jsBuilder.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"unsafe" // We seem to be using this alot lmao
+	"os"
+	"unsafe"
 )
 
 func addJsLine(TSCode *string, line string) {
 	*TSCode += line + "\n"
 }
 
-func createJSData(config *TG_Config, palletData *TG_Pallet_Data) {
-	var TSCode = "export const: string[] blockPallet = [" // Start of the pallet
+func createJSDataNonRLE(config *TG_Config, palletData *TG_Pallet_Data) {
+	var TSCode = "export const blockPallet: string[] = [" // Start of the pallet
 	for _, blockName := range palletData.pallet {
 		TSCode += "\"" + blockName + "\","
 	}
@@ -20,43 +20,36 @@ func createJSData(config *TG_Config, palletData *TG_Pallet_Data) {
 	TSCode += "];\n" // End of the pallet
 	print(TSCode)
 	// Add used data to the TSCode
-	addJsLine(&TSCode, "export const: number chunkWidth = "+fmt.Sprint(config.XSize)+";")
-	addJsLine(&TSCode, "export const: number chunkDepth = "+fmt.Sprint(config.ZSize)+";")
-	addJsLine(&TSCode, "export const: number chunkHeight = "+fmt.Sprint(ChunkHeight)+";")
+	addJsLine(&TSCode, "export const chunkWidth: number = "+fmt.Sprint(config.XSize)+";")
+	addJsLine(&TSCode, "export const chunkDepth: number = "+fmt.Sprint(config.ZSize)+";")
+	addJsLine(&TSCode, "export const chunkHeight: number = "+fmt.Sprint(ChunkHeight)+";")
 	print(TSCode)
 	// Add the chunk data
-	addJsLine(&TSCode, "export const: string[][] chunkData = [\n") // Start of the chunk data this could get thick
+	addJsLine(&TSCode, "export const chunkData: string[][]  = [\n") // Start of the chunk data this could get thick
 	for _, chunk := range WorldChunks {
 		addJsLine(&TSCode, "[")
 
-		// Point to the start of the chunk data in memory
-		// This is risky but it should work
-		// It is also very fast
-		// We can just tell TS to read the string as hex
-		// This is a very hacky way of doing this but it should work
-		PointerToChunkData := uintptr(unsafe.Pointer(&chunk.BlockData[0]))
-		// Convert the pointer to a byte array
-		ResultingByteArray := (*[(ChunkWidth * ChunkHeight * ChunkDepth) * 2]byte)(unsafe.Pointer(PointerToChunkData))
+		var arrayAsBytes [(ChunkWidth * ChunkHeight * ChunkDepth) * 4]byte
+		for index, block := range chunk.BlockData {
+			ourBytes := [4]byte{0x22, 0x61, 0x22, 0x2C}
+			ourBytes[1] += byte(block)
+			arrayAsBytes[index*4] = ourBytes[0]
+			arrayAsBytes[index*4+1] = ourBytes[1]
+			arrayAsBytes[index*4+2] = ourBytes[2]
+			arrayAsBytes[index*4+3] = ourBytes[3]
+		}
+		// Obtain a pointer to the array
+		unsafeAABpointer := unsafe.Pointer(&arrayAsBytes)
+		// convert that pointer to a byte pointer
+		unsafeAABpointerAsBytePointer := (*byte)(unsafeAABpointer)
 
-		// Set every second byte to 0x2C (,) this is the seperator for the hex values
-		for i := 1; i < len(ResultingByteArray); i += 2 {
-			ResultingByteArray[i] = 0x2C
-		}
-		// Add 33 to every other byte this is to make the hex values valid and can be shown as a string
-		for i := 0; i < len(ResultingByteArray); i += 2 {
-			ResultingByteArray[i] += 33
-		}
-
-		PointerToByteArray := uintptr(unsafe.Pointer(&ResultingByteArray[0]))
-		stringHeader := &reflect.StringHeader{
-			Data: PointerToByteArray,
-			Len:  (ChunkWidth * ChunkHeight * ChunkDepth) * 2,
-		}
-		resultingString := *(*string)(unsafe.Pointer(stringHeader))
-		addJsLine(&TSCode, resultingString)
+		addJsLine(&TSCode, unsafe.String(unsafeAABpointerAsBytePointer, ((ChunkWidth*ChunkHeight*ChunkDepth)*4)-1))
 		addJsLine(&TSCode, "],")
-		// print(TSCode)
+		//print(TSCode)
 	}
+	TSCode = TSCode[:len(TSCode)-2]
 	addJsLine(&TSCode, "];\n") // End of the chunk data
-	print(TSCode)
+
+	// Write the file
+	os.WriteFile(config.JSOutputPath, []byte(TSCode), 0644)
 }
