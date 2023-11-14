50666d1b30f0d13675942917ade9d6a5f975f40c
Duckos-Mods <businwes@hotmail.com>
2023-11-14 11:20:16 +0000

    added a better JS expoerter

--- main/jsBuilder.go
+++ main/jsBuilder.go
@@ -9,8 +9,23 @@ func createSetblockCommand(arrayBuf *[]byte, x uint32, y uint32, z uint32, block
 	*arrayBuf = append(*arrayBuf, []byte(fmt.Sprintf("\"setblock %d %d %d %s\",", x, y, z, palletData.pallet[blockID]))...)
 }
 
+func createFillBlockCommand(arrayBuf *[]byte, x uint32, y uint32, z uint32, blockID uint16, palletData *TG_Pallet_Data) {
+	*arrayBuf = append(*arrayBuf, []byte(fmt.Sprintf("\"fill %d %d %d %d -64 %d %s\",", x, y, z, x, z, palletData.pallet[blockID]))...)
+}
+
 func addJsLine(JSCode *string, line string) {
 	*JSCode += line + "\n"
+}
+
+func addChunkPositionBuffer(arrayBuf *[]byte) {
+	*arrayBuf = append(*arrayBuf, []byte(fmt.Sprintf("export const chunkPositions = ["))...)
+	for _, chunk := range WorldChunks {
+		*arrayBuf = append(*arrayBuf, []byte(fmt.Sprintf("%d,%d,", chunk.ChunkPosition.x*16, chunk.ChunkPosition.z*16))...)
+	}
+	(*arrayBuf)[len(*arrayBuf)-1] = ']' // Remove trailing comma
+	*arrayBuf = append(*arrayBuf, []byte(";")...)
+
+	*arrayBuf = append(*arrayBuf, []byte(fmt.Sprintf("export const eachChunkSize = %d;", ChunkWidth*ChunkDepth*ChunkHeight))...)
 }
 
 func createJSDataNonRLE(config *TG_Config, palletData *TG_Pallet_Data) {
@@ -28,6 +43,24 @@ func createJSDataNonRLE(config *TG_Config, palletData *TG_Pallet_Data) {
 	}
 	outputJS[len(outputJS)-1] = ']' // Remove trailing comma
 	outputJS = append(outputJS, []byte(";")...)
+	addChunkPositionBuffer(&outputJS)
+	os.WriteFile(config.JSOutputPath, outputJS, 0644)
+}
+
+func createJSHeightMapVersion(config *TG_Config, palletData *TG_Pallet_Data) {
+	var outputJS []byte
+	outputJS = append(outputJS, []byte("export const commandBuff = [")...)
+	for _, chunk := range WorldChunks {
+		for x := int32(0); x < ChunkWidth; x++ {
+			for z := int32(0); z < ChunkDepth; z++ {
+				var blockID = chunk.BlockData[TG_3D_PosToIndex(TG_3D_Pos{x, int32(chunk.HeightMap[x+z*ChunkWidth]), z})]
+				createFillBlockCommand(&outputJS, uint32(x+chunk.ChunkPosition.x*ChunkWidth), uint32(chunk.HeightMap[x+z*ChunkWidth]), uint32(z+chunk.ChunkPosition.z*ChunkDepth), blockID, palletData)
+			}
+		}
+	}
+	outputJS[len(outputJS)-1] = ']' // Remove trailing comma
+	outputJS = append(outputJS, []byte(";")...)
+	addChunkPositionBuffer(&outputJS)
 	os.WriteFile(config.JSOutputPath, outputJS, 0644)
 }
 
--- main/main.go
+++ main/main.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println("Welcome to Terrain Genie!")
 	var config TG_Config
 	var generateJS bool = true
+	var generateHeightMapVer bool = false
 
 	flag.StringVar(&config.OutputPath, "output", "./levelData", "Output path for generated terrain data")
 	flag.StringVar(&config.JSOutputPath, "TSOutput", "./levelData.js", "Output path for the generated JavaScript file (This is only used if the -JS flag is set)")
@@ -17,10 +18,11 @@ func main() {
 	flag.IntVar(&config.XSize, "X", 4, "The size in CHUNKS of the X axis of the world")
 	flag.IntVar(&config.ZSize, "Z", 4, "The size in CHUNKS of the Z axis of the world")
 	flag.IntVar(&config.YSize, "Y", 256, "The size in BLOCKS of the Y axis of the world")
-	flag.BoolVar(&config.OutputNonCompressed, "ONC", false, "Output non compressed binary data")
+	flag.BoolVar(&config.OutputNonCompressed, "ONC", true, "Output non compressed binary data")
 	flag.BoolVar(&config.EnableRLE, "RLE", false, "This is experimental and may not work")
 	flag.BoolVar(&generateJS, "JS", true, "Generate JavaScript from the world data")
-
+	flag.BoolVar(&generateHeightMapVer, "GMV", true, "Generate JavaScript from the world data only exporting the height map this makes smaller filles but not caves")
+	config.Seed = 10
 	flag.Parse()
 
 	fmt.Println("Output path:", config.OutputPath)
@@ -41,7 +43,11 @@ func main() {
 		if config.EnableRLE {
 			createJSDataRLE(&config, &palletData)
 		} else {
-			createJSDataNonRLE(&config, &palletData)
+			if generateHeightMapVer {
+				createJSHeightMapVersion(&config, &palletData)
+			} else {
+				createJSDataNonRLE(&config, &palletData)
+			}
 		}
 	}
 }
