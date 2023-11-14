1a10e46e831f736c0a2f6c59e8fa1fdb347e6cb2
Duckos-Mods <businwes@hotmail.com>
2023-11-14 11:21:26 +0000

    Reworked the JS importer

--- TerrainGenieImporter/scripts/main.js
+++ TerrainGenieImporter/scripts/main.js
@@ -1,23 +1,35 @@
 import { world, system } from '@minecraft/server';
-import { commandBuff, RLE } from './levelData.js';
+import { commandBuff, chunkPositions, eachChunkSize } from './levelData.js';
 
 
-function dateTime() {return globalThis.__date_clock()}
+function dateTime() { return globalThis.__date_clock() }
 
 let lastIndex = 0;
 let startTime = 0
+let lastChunk = 0
 
+const realChunkSize = 256 // Hard Coded for now should be set by the level data
 
 function placeBlockData(player) {
     startTime = dateTime();
-
+    const tpPlayerToLatestChunkInTick = (player) => {
+        if (lastIndex >= commandBuff.length) return false;
+        if (lastIndex % (realChunkSize ) !== 0) return false;
+        const chunkIndex = Math.floor(lastIndex / realChunkSize) ;
+        if (lastChunk === chunkIndex) return false;
+        lastChunk = chunkIndex;
+        const chunkPosX = chunkPositions[chunkIndex * 2];
+        const chunkPosZ = chunkPositions[chunkIndex * 2 + 1];
+        system.run(() => player.teleport({ x: chunkPosX, y: 100, z: chunkPosZ }));
+        return true;
+    }
     for (let i = lastIndex; i < commandBuff.length; i++) {
         const command = commandBuff[i];
         system.run(() => player.runCommand(command));
         lastIndex = i;
-        if (dateTime() - startTime > 200000) {
-            console.log(`Last index: ${lastIndex}`);
+        if (dateTime() - startTime > 180000 || tpPlayerToLatestChunkInTick(player)) {
             const callLambda = () => placeBlockData(player);
+            tpPlayerToLatestChunkInTick(player);
             system.run(callLambda)
             break;
         }
